a387374befd8eceaedc8dbdc145f8aa802eaf421
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-11-29 16:11:09 -0200

    install tinymce for angular in ionic4 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, EditorModule],
   providers: [
     StatusBar,
     SplashScreen,
